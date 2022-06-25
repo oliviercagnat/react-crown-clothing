7bd17303ec1d58ba2b57087f09f8447801e5ac10
oliviercagnat <oliviercagnat@gmail.com>
2022-06-25 12:12:41 +0900

    add shop

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.scss';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from './contexts/products.context';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
